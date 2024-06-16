4db3ce2a1501453c9d04fbb31dc28849904e1f82
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-06-16 19:09:09 +0200

    Simple Pig Latin

--- SimplePigLatin.js
+++ SimplePigLatin.js
@@ -1,3 +1,5 @@
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
 function pigIt(str) {
   let newarr = [],
     sliced = "";
@@ -10,3 +12,5 @@ function pigIt(str) {
   });
   return newarr.join(" ");
 }
+pigIt("Pig latin is cool"); // igPay atinlay siay oolcay
+pigIt("Hello world !"); // elloHay orldway !
